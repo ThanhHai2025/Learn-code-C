75ff1eeea6e5e3a4df5dc0d360359db8cb42fe06
ThanhHai2025 <haingothak@gmail.com>
2025-06-01 21:14:05 +0700

    cơ bản

---
+++ vidu1-C++.cpp
@@ -0,0 +1,17 @@
+// Phan 1: Dinh kem cac thu vien
+#include<stdio.h>
+
+// Phan 2: Phan bien
+
+// Phan 3: cac ham, phuong thuc
+int main () {
+	unsigned short tuoi;
+	int so_tien_co;
+	int so_tien  = 30000;
+	char c= 'A';
+	long dan_so = 100000000;
+	double dien_tich;
+	float dien_tich_tam_giac;
+	float x1, x2;
+	float b,c;
+}
